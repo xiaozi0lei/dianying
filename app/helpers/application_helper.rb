dee097d590c419fa8258c91e44123f328a45188d
gusun1 <gusun@redhat.com>
2014-10-09 11:36:14 +0800

    add support for table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,11 +11,11 @@ module ApplicationHelper
     coderayified = CodeRayify.new(:filter_html => true, 
                                   :hard_wrap => true)
     options = {
-      :fenced_code_blocks => true,
       :no_intra_emphasis => true,
+      :tables => true,
+      :fenced_code_blocks => true,
       :autolink => true,
       :strikethrough => true,
-      :lax_html_blocks => true,
       :superscript => true
     }
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
