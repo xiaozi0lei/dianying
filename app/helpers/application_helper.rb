a6084be0c7d656c0e54ebebb654a8b4002fa0751
xiaozi0lei <xiaozi0lei@163.com>
2014-10-13 02:11:13 +0800

    change to use bootstrap front end toolkit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,29 @@
 module ApplicationHelper
 
-  class CodeRayify < Redcarpet::Render::HTML
-    def block_code(code, language)
-      #CodeRay.scan(code, language).div
-      CodeRay.scan(code, language).div(:line_numbers => :table)
-    end
+class PygmentsHTML < Redcarpet::Render::HTML
+  def block_code code, language
+    Pygments.highlight(code, :lexer => language)
   end
-  
+end
+
+#  class CodeRayify < Redcarpet::Render::HTML
+#    def block_code(code, language)
+#      #CodeRay.scan(code, language).div
+#      CodeRay.scan(code, language).div(:line_numbers => :table)
+#    end
+#  end
+
   def markdown(text)
-    coderayified = CodeRayify.new(:filter_html => true, 
-                                  :hard_wrap => true)
+#    coderayified = CodeRayify.new(:filter_html => true,:hard_wrap => true)
+    coderayified = PygmentsHTML.new(:filter_html => true,:hard_wrap => true)
+#    coderayified = Redcarpet::Render::HTML.new(:filter_html => true,:hard_wrap => true)
     options = {
       :no_intra_emphasis => true,
       :tables => true,
       :fenced_code_blocks => true,
       :autolink => true,
       :strikethrough => true,
+      :lax_spacing => true,
       :superscript => true
     }
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
