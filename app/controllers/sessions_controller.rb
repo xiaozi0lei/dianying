1560ad1746608ff0454bbce44f0aa4d5f3a2039c
xiaozi0lei <xiaozi0lei@163.com>
2014-10-14 08:08:26 +0800

    update main page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_name] = user.email
       redirect_to root_url, :notice => "Logged in!"
     else
-      flash.now.alert = "Invalid email or password"
+      flash.now[:alert] = "Invalid email or password"
       render "new"
     end
   end
