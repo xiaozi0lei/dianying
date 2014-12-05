a2f1d221c9912f01f658f3dc0021106d61437f7d
xiaozi0lei <xiaozi0lei@163.com>
2014-12-05 19:55:45 +0800

    Picture store too hard 2

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 class Picture < ActiveRecord::Base
   # mount_uploader method must be called before after_save method.
   mount_uploader :picture, PictureUploader
@@ -8,8 +10,8 @@ class Picture < ActiveRecord::Base
 
     def get_picture_url
       # for openshift begin
-      self.picture_url(:thumb) =~ /data/
-      File.symlink(self.picture_url(:thumb),"#{Rails.root}/public#{$'}")
+      self.picture_url(:thumb) =~ /public/
+      FileUtils.cp_r($`,"#{Rails.root}/public/")
       # for openshift end
       #self[:path] = self.picture_url(:thumb)
     end
